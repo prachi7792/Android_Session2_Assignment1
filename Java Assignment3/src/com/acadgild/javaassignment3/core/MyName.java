db5df15da916438610e6c74c8edccaafe8cd6bd2
prachi7792 <prachipednekar85@gmail.com>
2017-04-08 01:05:08 +0530

    Print My Name
    This Assignment says to print my name without using semicolon

---
+++ Java Assignment3/src/com/acadgild/javaassignment3/core/MyName.java
@@ -0,0 +1,15 @@
+package com.acadgild.javaassignment3.core;
+
+public class MyName {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		if(System.out.printf("Prachi Pednekar!\n")== null)
+		{
+
+		}
+	
+
+	}
+
+}
